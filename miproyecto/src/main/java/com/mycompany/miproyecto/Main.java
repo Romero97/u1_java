6b4d9cd9f897e73c5672d45e241f13c7edfabe94
alumno <alumno@AR2-12.ir.uqroo.mx>
2019-09-02 16:10:58 -0500

    prueba 2

--- miproyecto/src/main/java/com/mycompany/miproyecto/Main.java
+++ miproyecto/src/main/java/com/mycompany/miproyecto/Main.java
@@ -15,6 +15,6 @@ public class Main {
         System.out.println("holaa");
         System.out.println("holaa");
         System.out.println("holaa mundooooo");
-        System.out.println("siuuuuuuuuuuuuuuuuu");
+        
     }
 }
